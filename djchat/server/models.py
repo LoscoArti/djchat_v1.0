c089f0b1c4c870fcaf00debcad2a17b785298a76
LoscoArti <loscoarti@gmail.com>
2023-06-29 13:53:50 +0200

    Updated views and serializer of Server model

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -22,7 +22,7 @@ class Server(models.Model):
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} id({self.id})"
 
 
 class Channel(models.Model):
@@ -31,7 +31,9 @@ class Channel(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
     )
     topic = models.CharField(max_length=100)
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="channel_server"
+    )
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
@@ -10,9 +12,39 @@ class ServerListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
+
+        if by_user or by_serverid and not request.user.is_authenticated:
+            raise AuthenticationFailed(detail="User must be authenticated")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        if by_user:
+            self.queryset = self.queryset.filter(member__id=request.user.id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset:
+                    raise ValidationError(
+                        detail=f"Server with id {by_serverid} not found"
+                    )
+            except ValueError:
+                raise ValidationError(
+                    detail=f"Invalid value for serverid: {by_serverid}"
+                )
+
+        serializer = ServerSerializer(
+            self.queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)
